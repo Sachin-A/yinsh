333b4b9f9a2e773d0000772be15dd951c9f2b2e4
shilpasweth <shilpasweth@gmail.com>
2018-03-11 02:20:57 +0530

    Reached Infnite Gameplay without constraints

--- js/Yinsh.js
+++ js/Yinsh.js
@@ -1,13 +1,19 @@
 var dimension = [document.documentElement.clientWidth, document.documentElement.clientHeight];
-var game_canvas = document.getElementById('GameBoard');
 
+var game_canvas = document.getElementById('GameBoard');
 game_canvas.width = dimension[1]*0.75;
 game_canvas.height = dimension[1]*0.75;
+var game_ctx = game_canvas.getContext("2d");
 
 var piece_canvas = document.getElementById('PieceLayer');
-
 piece_canvas.width = dimension[1]*0.75;
 piece_canvas.height = dimension[1]*0.75;
+var piece_ctx = piece_canvas.getContext("2d");
+
+var guide_canvas = document.getElementById('GuideLayer');
+guide_canvas.width = dimension[1]*0.75;
+guide_canvas.height = dimension[1]*0.75;
+var guide_ctx = guide_canvas.getContext("2d");
 
 var centerx = game_canvas.width/2;
 var centery = game_canvas.width/2;
@@ -15,13 +21,37 @@ var centery = game_canvas.width/2;
 var spacing = game_canvas.height/11;
 var altitude=spacing*Math.sqrt(3)/2;
 
-var game_ctx = game_canvas.getContext("2d");
+var current_player=0;
 
-var piece_ctx = piece_canvas.getContext("2d");
+var player=new Array(2);
+player[0]={board_rings:0, rings_won:0, color:"#00FF00", current_ring:[-1,-1]};
+player[1]={board_rings:0, rings_won:0, color:"#FF0000", current_ring:[-1,-1]};
+
+/*
+0-Place Rings
+1-Select Ring
+2-Move Ring
+3-Remove Row
+4-Remove Ring
+*/
+var required_move=0;
+
+function SwitchPlayer(){
+	current_player=(current_player+1)%2;
+}
+
+/*
+0-empty
+-1,1-player tokens
+-2,2-player rings
+
+*/
 
 function Point(x, y) {
   this.x = x;
   this.y = y;
+  this.piece=0;
+  this.guide=false;
 }
 
 var positions = new Array(11);
@@ -74,6 +104,7 @@ function PlotPoints(){
 			}
 			
 			positions[i][j]= new Point(centerx+altitude*x,centery-spacing*(y-x/2));
+			positions[i][j].valid=true;
 		}
 	}
 	
@@ -222,6 +253,131 @@ PlotPoints();
 DrawBoardLines();
 //LineTest();
 
+function PlaceRings(xcoord,ycoord){
+	if(positions[xcoord][ycoord].piece==0){
+		piece_ctx.beginPath();
+		piece_ctx.strokeStyle=player[current_player].color;
+		piece_ctx.arc(positions[xcoord][ycoord].x,positions[xcoord][ycoord].y,altitude/2,0,Math.PI*2);
+		piece_ctx.stroke();
+		positions[xcoord][ycoord].piece=Math.pow(-1,current_player)*2;
+		player[current_player].board_rings++;
+		if(player[current_player].board_rings==5&&player[(current_player+1)%2].board_rings==5){
+				required_move=1;
+		}
+		SwitchPlayer();
+	}
+}
+
+function BlackGuides(xcoord,ycoord,asign,bsign){
+	var token_line=0;
+	for(var a=asign, b=bsign;xcoord+a>=0&&xcoord+a<11&&ycoord+b>=0&&ycoord+b<11
+		&&Math.abs(positions[xcoord+a][ycoord+b].piece)!=2&&positions[xcoord+a][ycoord+b].x!=-1;a+=asign,b+=bsign){
+		if(positions[xcoord+a][ycoord+b].piece!=0){
+			token_line=1;
+			continue;
+		}
+		guide_ctx.beginPath();
+		guide_ctx.strokeStyle="black";
+		guide_ctx.arc(positions[xcoord+a][ycoord+b].x,positions[xcoord+a][ycoord+b].y,altitude/8,0,Math.PI*2);
+		guide_ctx.fillStyle = "black";
+		guide_ctx.fill();
+		guide_ctx.stroke();
+		positions[xcoord+a][ycoord+b].guide=true;
+		if(token_line==1){
+			break;
+		}
+	}
+
+}
+
+function SelectRings(xcoord,ycoord){
+	if(positions[xcoord][ycoord].piece==Math.pow(-1,current_player)*2){
+		guide_ctx.beginPath();
+		guide_ctx.strokeStyle="black";
+		guide_ctx.arc(positions[xcoord][ycoord].x,positions[xcoord][ycoord].y,altitude*3/8,0,Math.PI*2);
+		guide_ctx.fillStyle = player[current_player].color;
+		guide_ctx.fill();
+		guide_ctx.stroke();
+		player[current_player].current_ring=[xcoord,ycoord];
+		
+		BlackGuides(xcoord,ycoord,1,1);
+		BlackGuides(xcoord,ycoord,-1,-1);
+		BlackGuides(xcoord,ycoord,0,1);
+		BlackGuides(xcoord,ycoord,1,0);
+		BlackGuides(xcoord,ycoord,0,-1);
+		BlackGuides(xcoord,ycoord,-1,0);
+		
+		required_move=2;
+	}
+}
+
+function RemoveBlackGuides(xring,yring,destx,desty,asign,bsign){
+	var flip=0;
+	if(Math.sign(destx-xring)==Math.sign(asign)&&Math.sign(desty-yring)==Math.sign(bsign)){
+		flip=1;
+	}
+	for(var a=asign, b=bsign;xring+a>=0&&xring+a<11&&yring+b>=0&&yring+b<11
+		&&Math.abs(positions[xring+a][yring+b].piece)!=2&&positions[xring+a][yring+b].x!=-1;a+=asign,b+=bsign){
+		if(positions[xring+a][yring+b].piece==0){
+			positions[xring+a][yring+b].guide=false;
+			if(xring+a==destx&&yring+b==desty){
+
+				piece_ctx.clearRect(positions[xring][yring].x-altitude/1.9, positions[xring][yring].y-altitude/1.9
+					, altitude*1.1, altitude*1.1);
+
+				positions[xring][yring].piece=Math.pow(-1,current_player);
+				piece_ctx.beginPath();
+				piece_ctx.strokeStyle="black";
+				piece_ctx.arc(positions[xring][yring].x,positions[xring][yring].y,altitude*3/8,0,Math.PI*2);
+				piece_ctx.fillStyle = player[current_player].color;
+				piece_ctx.fill();
+				piece_ctx.stroke();
+
+				positions[destx][desty].piece=Math.pow(-1,current_player)*2;
+				piece_ctx.beginPath();
+				piece_ctx.strokeStyle=player[current_player].color;
+				piece_ctx.arc(positions[destx][desty].x,positions[destx][desty].y,altitude/2,0,Math.PI*2);
+				piece_ctx.stroke();
+
+				flip=0;
+			}
+
+		}
+		if(flip==1&&Math.abs(positions[xring+a][yring+b].piece)==1){
+			positions[xring+a][yring+b].piece*=-1;
+			piece_ctx.beginPath();
+			piece_ctx.strokeStyle="black";
+			piece_ctx.arc(positions[xring+a][yring+b].x,positions[xring+a][yring+b].y,altitude*3/8,0,Math.PI*2);
+			if(positions[xring+a][yring+b].piece==1){
+				piece_ctx.fillStyle = player[0].color;
+			}
+			else{
+				piece_ctx.fillStyle = player[1].color;
+			}
+			
+			piece_ctx.fill();
+			piece_ctx.stroke();
+		}
+		
+	}
+}
+
+function MoveRings(xcoord,ycoord){
+	if(positions[xcoord][ycoord].guide==true){
+		guide_ctx.clearRect(0, 0, guide_canvas.width, guide_canvas.height);
+		RemoveBlackGuides(player[current_player].current_ring[0],player[current_player].current_ring[1],xcoord,ycoord,1,1);
+		RemoveBlackGuides(player[current_player].current_ring[0],player[current_player].current_ring[1],xcoord,ycoord,1,0);
+		RemoveBlackGuides(player[current_player].current_ring[0],player[current_player].current_ring[1],xcoord,ycoord,0,1);
+		RemoveBlackGuides(player[current_player].current_ring[0],player[current_player].current_ring[1],xcoord,ycoord,-1,0);
+		RemoveBlackGuides(player[current_player].current_ring[0],player[current_player].current_ring[1],xcoord,ycoord,0,-1);
+		RemoveBlackGuides(player[current_player].current_ring[0],player[current_player].current_ring[1],xcoord,ycoord,-1,-1);
+		required_move=1;
+		SwitchPlayer();
+	}
+
+	
+}
+
 function IsClickValid(mouse){
 	for(var i=0;i<11;i++){
 		for(var j=0;j<11;j++){
@@ -230,9 +386,18 @@ function IsClickValid(mouse){
 			}
 			if(positions[i][j].x-altitude/2<mouse.x&&positions[i][j].x+altitude/2>mouse.x
 				&&positions[i][j].y-altitude/2<mouse.y&&positions[i][j].y+altitude/2>mouse.y){
-					piece_ctx.beginPath();
-					piece_ctx.arc(positions[i][j].x,positions[i][j].y,altitude/2,0,Math.PI*2);
-					piece_ctx.stroke();
+					if(required_move==0){
+						PlaceRings(i,j);
+						
+					}
+					else if(required_move==1){
+						SelectRings(i,j);
+						
+					}
+					else if(required_move==2){
+						MoveRings(i,j);
+						
+					}
 			}
 		}
 	}
